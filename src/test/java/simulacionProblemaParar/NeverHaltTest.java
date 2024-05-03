1fff3464f89345f7df8e7a93cec21dc171d601fb
Sira <sgonzmad@myuax.com>
2024-05-03 10:32:35 +0200

    NeverHaltTest.java

---
+++ src/test/java/simulacionProblemaParar/NeverHaltTest.java
@@ -0,0 +1,12 @@
+package simulacionProblemaParar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NeverHaltTest {
+
+    @Test
+    void main() {
+    }
+}
