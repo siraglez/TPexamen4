66a1c9f78d4cabd0e492603084312e863a2b6757
Sira <sgonzmad@myuax.com>
2024-05-08 19:38:06 +0200

    modificación comprobación entrada HaltChecker

--- src/main/java/comprobacionProblemaParar/HaltChecker.java
+++ src/main/java/comprobacionProblemaParar/HaltChecker.java
@@ -3,10 +3,11 @@ package comprobacionProblemaParar;
 //Implementación de un método que comprueba si el programa se detiene o no
 public class HaltChecker {
     public static boolean checkHalt(String codigoPrograma, String input) {
-        //Implementación simulada del HaltChecker
-        //En este ejemplo, se verifica si el programa cuenta hacia abajo desde un número específico
-        //Si la entrada es 10, asumimos que el programa se detendrá
-        if (codigoPrograma.equals("countDown") && input.equals("10")) {
+        //Simulación del HaltChecker
+        //Verifica si el programa cuenta hacia abajo desde un número específico
+        //Si la entrada es mayor que 0, asumimos que el programa se detendrá
+        int inputNumber = Integer.parseInt(input);
+        if (codigoPrograma.equals("countDown") && inputNumber > 0) {
             return true;
         }
         //De lo contrario, asumimos que el programa no se detiene
